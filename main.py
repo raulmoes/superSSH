765a10d39d70797e2531b3ca6760a0040d91376d
raulmoes <raulillomoesp21@gmail.com>
2022-10-11 20:00:46 +0200

    Editado Raul Morales

--- main.py
+++ main.py
@@ -1,43 +1,27 @@
-from functions import tryconnect, connectpassword
-from paramiko import SSHClient, AutoAddPolicy, RSAKey
-from scp import SCPClient
-from os import path
-import io
-import sys
+import os
+import paramiko
+sel = input ("Seleccione 1 para ejecutar un comando o 2 para copiar fichero del remotehost: ")
 
-user = "root"
-keypath = "/root/.ssh/root"
-remotehost = "192.168.58.49"
-defaultpassword = "root"
-ssh = SSHClient()
-ssh.set_missing_host_key_policy(AutoAddPolicy)
+if (sel == "1"):
+  pkey = paramiko.RSAKey.from_private_key_file('/home/frodo/.ssh/frodo', password='bolson')
+  trans = paramiko.Transport(('192.168.58.191', 22))
+  trans.connect(username='frodo', pkey=pkey)
+  ssh = paramiko.SSHClient()
+  ssh._transport = trans
+  comando = input ("Indica el comando a ejecutar: ")
+  stdin, stdout, stderr = ssh.exec_command(comando)
+  print(stdout.read().decode())
+  del stdin, stdout, stderr
+  trans.close()
 
-print("Welcome to the ssh master")
-remotehost = input("Give your remote server IP (192.168.58.49): ") or remotehost
-defaultpassword = input("Give the default password (root): ") or defaultpassword
-user = input("Give the default username (root): ") or user
-keypath = input("Give the path for the key (/root/.ssh/root): ") or keypath
-localpath = input("Give the path for the local file to copy (key path)") or keypath
-extpath = input("Give the path where you want to put the file (key path)") or keypath
-print("Will start the ssh connection")
-
-if path.exists(keypath):
-    key = RSAKey.from_private_key_file(keypath)
-    i = tryconnect(ssh, remotehost, user, key, defaultpassword)
-    print(i)
-    if i == 0:
-        sys.exit()
-else:
-    i = connectpassword(ssh, remotehost, user, defaultpassword)
-    print(i)
-    if i == 0:
-        sys.exit()
-print("hoalaaa")
-if path.exists(localpath):
-    print("holaaa")
-    scp = SCPClient(ssh.get_transport())
-    scp.put(localpath, extpath)
-    ssh.exec_command("cat extpath")
-    scp.close()
-
-ssh.close()
+elif (sel == "2"):
+    pkey = paramiko.RSAKey.from_private_key_file('/home/frodo/.ssh/frodo', password='bolson')
+    trans = paramiko.Transport(('192.168.58.191', 22))
+    trans.connect(username='frodo', password='bolson')
+    ssh = paramiko.SSHClient()
+    ssh._transport = trans
+    sftp = paramiko.SFTPClient.from_transport(trans)
+    local = input("Indique el fichero / carpeta local a copiar: ")
+    remoto = input("Indique la carpeta remota: ")
+    sftp.put(localpath=local, remotepath=remoto)
+    trans.close()
